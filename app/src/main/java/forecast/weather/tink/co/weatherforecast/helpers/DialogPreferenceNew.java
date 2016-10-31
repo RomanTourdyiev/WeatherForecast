79681636814b3f281dcd24f2d525ed8eb4d60dd0
CantadorRDA <cantadorcapo@gmail.com>
2016-10-31 17:22:35 +0200

    first commit

--- app/src/main/java/forecast/weather/tink/co/weatherforecast/helpers/DialogPreferenceNew.java
+++ app/src/main/java/forecast/weather/tink/co/weatherforecast/helpers/DialogPreferenceNew.java
@@ -1,7 +1,7 @@
 package forecast.weather.tink.co.weatherforecast.helpers;
 
 import android.content.Context;
-import android.support.v7.preference.DialogPreference;
+import android.preference.DialogPreference;
 import android.util.AttributeSet;
 import android.view.View;
 
