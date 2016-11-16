d6c3d7168d7f1d610b70919abc4814a73ce6d1f7
CantadorRDA <cantadorcapo@gmail.com>
2016-11-16 23:50:15 +0200

    first commit

---
+++ app/src/main/java/forecast/weather/tink/co/weatherforecast/views/rangebar/IRangeBarFormatter.java
@@ -0,0 +1,10 @@
+package forecast.weather.tink.co.weatherforecast.views.rangebar;
+
+/**
+ * Created by franciscomorais on 24/06/15.
+ */
+public interface IRangeBarFormatter {
+
+    String format(String value);
+
+}
